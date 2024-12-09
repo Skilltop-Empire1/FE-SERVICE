494e767eb332cf9f3d12899b94f6eb0340c03d2e
marvellous <comlaf2001@gmail.com>
2024-12-09 15:00:06 +0100

    Add modal added for reusability

--- src/features/reusables/AddModal.jsx
+++ src/features/reusables/AddModal.jsx
@@ -1,26 +1,43 @@
 import React from 'react'
 import style from './addmodalStyle.module.css'
+import FilledButton from './Buttons/FilledButton'
+import OutlinedButton from './Buttons/OutlinedButton'
 
-const AddModal = () => {
+const AddModal = ({header, formContent, close, submit, addAnother, anotherContent}) => {
   return (
-    <div>
-       <div className="mt-8 flex items-center gap-4">
-            <input
-              type="checkbox"
-              name="check"
-            //   checked={addAnother}
-              onChange={(e) => setAddAnother(e.target.checked)}
-              className={`${style.check} flex items-center justify-center`}
-            />
-            <label htmlFor="check">Add another product</label>
-          </div>
+    <div className={style.body}>
 
-          <div className="mt-5">
-            <button type="submit" className={style.submit} >
-              {/* {isLoading ? 'Saving...' : saveMessage ? 'Saved' : 'Save product'} */}
-              save
-            </button>
-          </div>
+      <div className={style.top}>
+        <h2 className={style.title}>{header}</h2>
+      </div>
+
+
+       <form action="" >
+
+        {/* form content passed as a prop */}
+        <div className={style.form}>
+          {formContent}
+        </div>
+        
+        {/* button and checkbox to add another */}
+        <div className='grid-cols-2 grid'>
+            <div className="mt-8 flex items-center gap-4">
+                  <input
+                    type="checkbox"
+                    name="check"
+                  //   checked={addAnother}
+                    onChange={addAnother}
+                    className={`${style.check} flex items-center justify-center`}
+                  />
+                  <label htmlFor="check">{anotherContent}</label>
+                </div>
+
+                <div className="mt-5 flex gap-3">
+                  <OutlinedButton content={'Cancel'} performAction={close}/>
+                  <FilledButton content={'Save'} performAction={submit}/>
+                </div>
+        </div>
+       </form>
     </div>
   )
 }
--- src/pages/addService/AddService.jsx
+++ src/pages/addService/AddService.jsx
@@ -1,10 +1,78 @@
 import React from 'react'
 import AddModal from '../../features/reusables/AddModal'
+import { useNavigate } from 'react-router'
 
 const AddService = () => {
+  const navigate = useNavigate()
+
+  const goBack = () => [
+    navigate('/app/Services')
+  ]
+
+  const formContent = 
+  <>
+    <div >
+            <label >Payment Option</label>
+            <select
+              required
+            >
+              <option value="">Select payment option</option>
+              <option value="full">Full Payment</option>
+              <option value="part_payment">Part Payment</option>
+              <option value="credit">Credit Sales</option>
+            </select>
+          </div>
+          <div>
+            <label>Payment Option</label>
+            <select
+
+              required
+            >
+              <option value="">Select payment option</option>
+              <option value="full">Full Payment</option>
+              <option value="part_payment">Part Payment</option>
+              <option value="credit">Credit Sales</option>
+            </select>
+          </div>
+          <div >
+            <label>Payment Option</label>
+            <select
+              required
+            >
+              <option value="">Select payment option</option>
+              <option value="full">Full Payment</option>
+              <option value="part_payment">Part Payment</option>
+              <option value="credit">Credit Sales</option>
+            </select>
+          </div>
+          <div >
+            <label >Quantity:</label>
+            <input
+              type="number"
+              required
+            />
+          </div>
+          <div >
+            <label >Quantity:</label>
+            <input
+              type="number"
+              required
+            />
+          </div>
+  </>
+
+
+
   return (
     <div>
-        <AddModal/>
+        {/* use the addAnother to implement the logic to remain on the page if checked */}
+        <AddModal
+          header={'Add Sales Record'} 
+          formContent={formContent} 
+          close={goBack} 
+          anotherContent={'Add another service'}
+          
+        />
     </div>
   )
 }
