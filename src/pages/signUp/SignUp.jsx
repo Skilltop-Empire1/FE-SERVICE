7c922bc814ad43a56b178d9e84f92f32776b075b
eedreese <idbalorg@gmail.com>
2024-12-09 15:33:35 +0100

    login and sign up UIs

--- src/pages/signUp/SignUp.jsx
+++ src/pages/signUp/SignUp.jsx
@@ -1,5 +1,102 @@
+import React, { useState } from 'react'
+import signUpImage from '../../assets/signupIllustration.svg'
+import style from './SignUp.module.css'
+import { Link } from 'react-router-dom'
+
 function SignUp() {
-  return <div>SignUp</div>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    organization: '',
+    termsAccepted: false,
+  })
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: type === 'checkbox' ? checked : value,
+    }))
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (!formData.termsAccepted) {
+      alert('You must accept the terms and conditions to sign up.')
+      return
+    }
+    console.log('Form submitted with data:', formData)
+  }
+
+  return (
+    <div className={style.container}>
+      <div className={style.illustrationWrapper}>
+        <div>
+          <img src={signUpImage} alt="Sign Up Illustration" />
+        </div>
+      </div>
+
+      <div className={style.formWrapper}>
+        <h2>Create an Account</h2>
+        <p>Create an Account to manage your team and tasks.</p>
+        <form onSubmit={handleSubmit}>
+          <div className={style.inputField}>
+            <label htmlFor="email">Email Address:</label>
+            <input
+              className={style.input}
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              placeholder="Enter your email address"
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={style.inputField}>
+            <label htmlFor="password">Password:</label>
+            <input
+              className={style.input}
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Enter your password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={style.inputField}>
+            <label htmlFor="organization">Organization Name:</label>
+            <input
+              className={style.input}
+              type="text"
+              id="organization"
+              name="organization"
+              placeholder="Enter your organization"
+              value={formData.organization}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className={style.checkboxField}>
+            <input
+              type="checkbox"
+              name="termsAccepted"
+              checked={formData.termsAccepted}
+              onChange={handleChange}
+              className={style.checkbox}
+            />
+            <label>By clicking, you agree to IMS terms and conditions</label>
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+        <p className={style.toLoginRoute}>
+          Already have an account? <Link to="/login">Sign In</Link>
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default SignUp
