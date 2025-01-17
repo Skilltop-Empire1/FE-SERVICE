d1fe0feaefba541e2cdb50209f1b58a5d2e4f0bc
papasunnee <papasunnee@users.noreply.github.com>
2025-01-17 13:19:47 +0100

    update add employee

--- src/pages/addEmployee/AddEmployee.jsx
+++ src/pages/addEmployee/AddEmployee.jsx
@@ -4,7 +4,7 @@ function AddEmployee() {
   return (
     <div className="flex flex-col gap-4">
       <div>
-        <h1 className="text-3xl font-bold">Employee Invite Settings</h1>
+        <h1 className="text-3xl font-bold">Employee Invite - Settings</h1>
       </div>
       <div className="my-4">
         <EmployeeInviteForm />
